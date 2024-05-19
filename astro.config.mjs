a98a75c5b4fe2e6798575a2a89c367f191d8ac79
Ahmed Waseem Saeed <ahmed99@live.no>
2024-05-20 00:32:38 +0200

    fix: base for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://github.com/ahmeds99/",
-  integrations: [tailwind()]
+  base: "personal-website",
+  integrations: [tailwind()],
 });
