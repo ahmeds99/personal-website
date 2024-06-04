0c53a55e0d6f6af2d1c52365ecc088e151ae282d
Ahmed Waseem Saeed <ahmed99@live.no>
2024-06-05 00:46:57 +0200

    hacky fix: show all images on click, todo: carousel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: vercel(),
   site: "https://github.com/ahmeds99/",
   base: "personal-website",
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()]
 });
